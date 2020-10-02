b10211d1805c6e1a2750f0ec02ca366dca9bc126
Paurush Kumar Gupta <gpaurush39@gmail.com>
2020-10-02 20:46:16 +0530

    Added Model

--- URLshortner/shortner/models.py
+++ URLshortner/shortner/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Url(models.Model):
+    url_id = models.AutoField(primary_key=True)
+    link = models.CharField(max_length=4095)
+    short_link = models.CharField(max_length=128)
